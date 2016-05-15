15e13f7a395a2fa0d9f5a6db3a83d52b561d8518
Etienne Perot <etienne@perot.me>
2016-05-15 00:38:51 -0700

    Implement file chunking and reading.

--- split/split.go
+++ split/split.go
@@ -4,11 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"hash/fnv"
+	"io"
 	"io/ioutil"
 	"math/big"
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -19,7 +22,7 @@ import (
 
 type splitFS struct {
 	sourceDirectory string
-	chunkSize       *big.Int
+	chunkSize       int64
 }
 
 var _ fs.FS = (*splitFS)(nil)
@@ -45,7 +48,7 @@ func NewFS(sourceDirectory string, chunkSize int64) (fs.FS, error) {
 	}
 	return &splitFS{
 		sourceDirectory: absoluteSource,
-		chunkSize:       big.NewInt(chunkSize),
+		chunkSize:       chunkSize,
 	}, nil
 }
 
@@ -58,19 +61,6 @@ func (n *node) FullPath() string {
 	return path.Join(n.splitFS.sourceDirectory, n.rootRelativePath)
 }
 
-type directory struct {
-	*node
-}
-
-var _ fs.Node = (*directory)(nil)
-var _ fs.Handle = (*directory)(nil)
-var _ fs.HandleReadDirAller = (*directory)(nil)
-
-func convertTime(timespec syscall.Timespec) time.Time {
-	sec, nsec := timespec.Unix()
-	return time.Unix(sec, nsec)
-}
-
 func osToFuseErr(err error) error {
 	if os.IsNotExist(err) {
 		return fuse.ENOENT
@@ -79,6 +69,11 @@ func osToFuseErr(err error) error {
 		return fuse.EPERM
 	}
 	return fuse.ENOSYS
+}
+
+func convertTime(timespec syscall.Timespec) time.Time {
+	sec, nsec := timespec.Unix()
+	return time.Unix(sec, nsec)
 }
 
 func copyStatToAttr(stat *syscall.Stat_t, attr *fuse.Attr) {
@@ -120,14 +115,21 @@ func copyStatToAttr(stat *syscall.Stat_t, attr *fuse.Attr) {
 	attr.Ctime = convertTime(stat.Ctim)
 }
 
-func (d *directory) Attr(_ context.Context, attr *fuse.Attr) error {
+func (n *node) Attr(_ context.Context, attr *fuse.Attr) error {
 	stat := &syscall.Stat_t{}
-	if err := syscall.Stat(d.FullPath(), stat); err != nil {
+	if err := syscall.Stat(n.FullPath(), stat); err != nil {
 		return osToFuseErr(err)
 	}
 	copyStatToAttr(stat, attr)
 	return nil
 }
+
+type directory struct {
+	*node
+}
+
+var _ fs.Node = (*directory)(nil)
+var _ fs.HandleReadDirAller = (*directory)(nil)
 
 func (d *directory) ReadDirAll(context.Context) ([]fuse.Dirent, error) {
 	files, err := ioutil.ReadDir(d.FullPath())
@@ -193,15 +195,12 @@ type fileAsDir struct {
 }
 
 var _ fs.Node = (*fileAsDir)(nil)
-var _ fs.Handle = (*fileAsDir)(nil)
 var _ fs.HandleReadDirAller = (*fileAsDir)(nil)
 
-func (f *fileAsDir) Attr(_ context.Context, attr *fuse.Attr) error {
-	stat := &syscall.Stat_t{}
-	if err := syscall.Stat(f.FullPath(), stat); err != nil {
-		return osToFuseErr(err)
+func (f *fileAsDir) Attr(ctx context.Context, attr *fuse.Attr) error {
+	if err := f.node.Attr(ctx, attr); err != nil {
+		return err
 	}
-	copyStatToAttr(stat, attr)
 	attr.Mode = (attr.Mode & 0555) | os.ModeDir
 	return nil
 }
@@ -210,24 +209,37 @@ const minFormatZeroes = 8
 
 var fileAsDirFormatString = fmt.Sprintf("%%x_%%0%dd_of_%%0%dd", minFormatZeroes, minFormatZeroes)
 
-func (f *fileAsDir) ReadDirAll(context.Context) ([]fuse.Dirent, error) {
+// ceilAndRemainder returns (ceil(x / y), x mod y).
+// It panics if y == 0.
+func ceilAndRemainder(x, y int64) (int64, int64) {
+	bigQ, bigR := big.NewInt(0).DivMod(big.NewInt(x), big.NewInt(y), big.NewInt(0))
+	q, r := bigQ.Int64(), bigR.Int64()
+	if r > 0 {
+		q++
+	}
+	return q, r
+}
+
+// getChunks returns (number of chunks, size of last chunk in bytes, error)
+func (f *fileAsDir) getChunks() (int64, int64, error) {
 	stat, err := os.Stat(f.FullPath())
+	if err != nil {
+		return 0, 0, err
+	}
+	numChunks, lastChunkSize := ceilAndRemainder(stat.Size(), f.splitFS.chunkSize)
+	return numChunks, lastChunkSize, nil
+}
+
+func (f *fileAsDir) ReadDirAll(context.Context) ([]fuse.Dirent, error) {
+	numChunks, _, err := f.getChunks()
 	if err != nil {
 		return nil, osToFuseErr(err)
 	}
-	size := big.NewInt(stat.Size())
-	quotient := big.NewInt(0)
-	remainder := big.NewInt(0)
-	quotient, remainder = quotient.DivMod(size, f.splitFS.chunkSize, remainder)
-	numChunks := quotient.Uint64()
-	if remainder.Uint64() > 0 {
-		numChunks++
-	}
 	entries := make([]fuse.Dirent, numChunks)
 	lastChunkNumber := numChunks - 1
-	for i := uint64(0); i < numChunks; i++ {
+	for i := int64(0); i < numChunks; i++ {
 		entries[i] = fuse.Dirent{
-			Inode: f.hash + i,
+			Inode: f.hash + uint64(i),
 			Type:  fuse.DT_File,
 			Name:  fmt.Sprintf(fileAsDirFormatString, f.hash, i, lastChunkNumber),
 		}
@@ -236,5 +248,117 @@ func (f *fileAsDir) ReadDirAll(context.Context) ([]fuse.Dirent, error) {
 }
 
 func (f *fileAsDir) Lookup(_ context.Context, name string) (fs.Node, error) {
-	return nil, errors.New("unimplemented")
+	parts := strings.Split(name, "_")
+	if len(parts) != 4 {
+		return nil, fuse.ENOENT
+	}
+	hashPart, chunkPart, ofPart, totalChunksPart := parts[0], parts[1], parts[2], parts[3]
+	if ofPart != "of" {
+		return nil, fuse.ENOENT
+	}
+	hash, err := strconv.ParseUint(hashPart, 16, 64)
+	if err != nil || hash != f.hash {
+		return nil, fuse.ENOENT
+	}
+	chunk, err := strconv.ParseInt(chunkPart, 10, 64)
+	if err != nil || chunk < 0 {
+		return nil, fuse.ENOENT
+	}
+	lastChunk, err := strconv.ParseInt(totalChunksPart, 10, 64)
+	if err != nil {
+		return nil, fuse.ENOENT
+	}
+	numChunks, lastChunkSize, err := f.getChunks()
+	if err != nil {
+		return nil, osToFuseErr(err)
+	}
+	if lastChunk+1 != numChunks || chunk >= numChunks {
+		return nil, fuse.ENOENT
+	}
+	size := f.splitFS.chunkSize
+	if chunk == lastChunk {
+		size = lastChunkSize
+	}
+	return &fileChunk{
+		node:   f.node,
+		offset: chunk * f.splitFS.chunkSize,
+		size:   size,
+	}, nil
+}
+
+var handleIDProvider <-chan fuse.HandleID
+
+func init() {
+	idProvider := make(chan fuse.HandleID)
+	handleIDProvider = idProvider
+	go func() {
+		for id := fuse.HandleID(2); ; id++ {
+			idProvider <- id
+		}
+	}()
+}
+
+type fileChunk struct {
+	*node
+	offset int64
+	size   int64
+}
+
+var _ fs.Node = (*fileChunk)(nil)
+var _ fs.NodeOpener = (*fileChunk)(nil)
+
+func (f *fileChunk) Attr(ctx context.Context, attr *fuse.Attr) error {
+	if err := f.node.Attr(ctx, attr); err != nil {
+		return err
+	}
+	attr.Size = uint64(f.size)
+	numBlocks, _ := ceilAndRemainder(f.size, 512)
+	attr.Blocks = uint64(numBlocks)
+	return nil
+}
+
+func (f *fileChunk) Open(_ context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
+	if !req.Flags.IsReadOnly() {
+		return nil, fuse.Errno(syscall.EROFS)
+	}
+	file, err := os.Open(f.FullPath())
+	if err != nil {
+		return nil, osToFuseErr(err)
+	}
+	if _, err := file.Seek(f.offset, 0); err != nil {
+		return nil, osToFuseErr(err)
+	}
+	resp.Handle = <-handleIDProvider
+	return &fileChunkHandle{f, file}, nil
+}
+
+type fileChunkHandle struct {
+	*fileChunk
+	file *os.File
+}
+
+var _ fs.Handle = (*fileChunkHandle)(nil)
+var _ fs.HandleReader = (*fileChunkHandle)(nil)
+var _ fs.HandleReleaser = (*fileChunkHandle)(nil)
+
+func (f *fileChunkHandle) Read(_ context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
+	trueOffset := req.Offset + f.offset
+	trueSize := int64(req.Size)
+	if trueSize < f.size-f.offset {
+		trueSize = f.size - f.offset
+	}
+	bytes := make([]byte, trueSize)
+	read, err := f.file.ReadAt(bytes, trueOffset)
+	if err != nil && err != io.EOF {
+		return osToFuseErr(err)
+	}
+	resp.Data = bytes[:read]
+	return nil
+}
+
+func (f *fileChunkHandle) Release(_ context.Context, req *fuse.ReleaseRequest) error {
+	if err := f.file.Close(); err != nil {
+		return osToFuseErr(err)
+	}
+	return nil
 }
